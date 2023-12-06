1c52264a511c8a593ee8af657619261b6bfb45bf
hoangducccc <111888463+hoangducccc@users.noreply.github.com>
2023-12-07 05:41:33 +0700

    Update client.c

--- client.c
+++ client.c
@@ -1,3 +1,4 @@
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -5,8 +6,9 @@
 #include <arpa/inet.h>
 #include <sys/stat.h>
 
-#define PORT 8080
-#define SERVER_IP "192.168.237.145"
+#define PORT 8081
+#define SERVER_IP "127.0.0.1"
+#define FILE_PATH "files/"
 
 void error(char *message) {
     perror(message);
@@ -20,11 +22,12 @@ typedef struct {
 
 void receiveFile(int socket_fd, char *filename) {
     char filepath[256];
-    snprintf(filepath, sizeof(filepath), "%s", filename);
+    snprintf(filepath, sizeof(filepath), "%s%s", FILE_PATH, filename);
 
-    FILE *file = fopen(filepath, "wb"); // Thay đổi chế độ mở tệp thành "wb"
+    FILE *file = fopen(filepath, "wb");  // Mở file để ghi dưới dạng binary
     if (file == NULL) {
-        error("Không thể tạo tệp");
+        perror("Không thể tạo tệp");
+        exit(EXIT_FAILURE);
     }
 
     char buffer[1024];
@@ -35,11 +38,48 @@ void receiveFile(int socket_fd, char *filename) {
         if (bytesRead <= 0) {
             break;
         }
-        fwrite(buffer, 1, bytesRead, file);
+        size_t bytesWritten = fwrite(buffer, 1, bytesRead, file);
+        if (bytesWritten != bytesRead) {
+            perror("Write to file failed");
+            fclose(file);
+            exit(EXIT_FAILURE);
+        }
     }
 
     fclose(file);
     printf("Đã nhận tệp %s\n", filename);
+}
+
+void upload_file(int socket_fd, char *filename) {
+    char filepath[256];
+    size_t bytesRead;
+    //printf("%s", filename);
+    snprintf(filepath, sizeof(filepath), "%s%s", FILE_PATH, filename);
+
+    FILE *file = fopen(filepath, "rb");  // Mở file để đọc dưới dạng binary
+    if (file == NULL) {
+        perror("Không thể mở tệp");
+        exit(EXIT_FAILURE);
+    }
+
+    char buffer[1024];
+
+    while ((bytesRead = fread(buffer, 1, sizeof(buffer), file)) > 0) {
+        ssize_t bytesSent = send(socket_fd, buffer, bytesRead, 0);
+        if (bytesSent == -1) {
+            perror("Lỗi khi gửi dữ liệu");
+            fclose(file);
+            exit(EXIT_FAILURE);
+        }
+        // Kiểm tra xem đã gửi đúng số byte hay không
+        if ((size_t)bytesSent != bytesRead) {
+            perror("Gửi không đủ dữ liệu");
+            fclose(file);
+            exit(EXIT_FAILURE);
+        }
+    }
+
+    fclose(file);
 }
 
 void listFiles(int socket_fd) {
@@ -57,6 +97,25 @@ void listFiles(int socket_fd) {
     }
 }
 
+int upload_confirm(int socket_fd) {
+    char buffer[1024];
+    ssize_t bytesRead;
+    int t = 1;
+
+    bytesRead = recv(socket_fd, buffer, sizeof(buffer), 0);
+    if (bytesRead <= 0) {
+        close(socket_fd);
+        return 0;
+    }
+    if (strcmp(buffer, "upload") == 0) {
+        printf("Done");
+        return 1;
+    }
+    else{
+        return 0;
+    }
+}
+
 void clearInputBuffer() {
     int c;
     while ((c = getchar()) != '\n' && c != EOF);
@@ -66,21 +125,16 @@ int main() {
     int socket_fd;
     struct sockaddr_in server_addr;
 
-    // Tạo socket
     socket_fd = socket(AF_INET, SOCK_STREAM, 0);
-    if (socket_fd == -1) {
-        error("Không thể tạo socket");
-    }
 
-    // Khởi tạo thông tin của server
+
     server_addr.sin_family = AF_INET;
     server_addr.sin_port = htons(PORT);
     inet_pton(AF_INET, SERVER_IP, &server_addr.sin_addr);
 
-    // Kết nối đến server
-    if (connect(socket_fd, (struct sockaddr *)&server_addr, sizeof(server_addr)) == -1) {
-        error("Không thể kết nối đến server");
-    }
+    //if (connect(socket_fd, (struct sockaddr *)&server_addr, sizeof(server_addr)) == -1) {
+    //    error("Không thể kết nối đến server");
+    //}
 
 
     while (1) {
@@ -88,66 +142,133 @@ int main() {
         printf("Lựa chọn:\n");
         printf("1. Xem danh sách tên tệp trong thư mục 'files' của server\n");
         printf("2. Tải tệp từ server\n");
-        printf("3. Thoát\n");
+        printf("3. Upload\n");
+        printf("4. Exit\n")
 
         int choice;
         scanf("%d", &choice);
-        clearInputBuffer(); // Consume newline characters
+        clearInputBuffer();
 
         switch (choice) {
             case 1: {
                 socket_fd = socket(AF_INET, SOCK_STREAM, 0);
                 connect(socket_fd, (struct sockaddr *)&server_addr, sizeof(server_addr));
                 listFiles(socket_fd);
+		        close(socket_fd);
                 break;
                 }
             case 2: {
                 int t = 0;
-                char request[] = "list";
+                char request[1024] = "list";
+                socket_fd = socket(AF_INET, SOCK_STREAM, 0);
+                connect(socket_fd, (struct sockaddr *)&server_addr, sizeof(server_addr));
                 send(socket_fd, request, strlen(request) + 1, 0);
                 SendFileList receiveList;
                 recv(socket_fd, &receiveList, sizeof(receiveList), 0);
+                close(socket_fd);
                 socket_fd = socket(AF_INET, SOCK_STREAM, 0);
                 connect(socket_fd, (struct sockaddr *)&server_addr, sizeof(server_addr));
                 while (t == 0) {
-                    // Nhập tên tệp cần tải
                     char filename[256];
-                    printf("Nhập tên tệp cần tải hoặc 'quit' để thoát: ");
+                    for (int i = 0; i < receiveList.num_files; i++) {
+                        printf("%s", receiveList.filenames[i]);}
+
+                    printf("Nhập tên file cần tải hoặc 'quit' để thoát: ");
                     fgets(filename, sizeof(filename), stdin);
 
-                    // Loại bỏ ký tự newline nếu có
                     size_t len = strlen(filename);
                     if (len > 0 && filename[len - 1] == '\n') {
                         filename[len - 1] = '\0';
                     }
 
-                    // Kiểm tra nếu người dùng muốn thoát
                     if (strcmp(filename, "quit") == 0) {
                         break;
                     }
 
-                    // Kiểm tra xem tên tệp có tồn tại trong danh sách
-                    int found = 0;
+
+                    int find = 0;
                     for (int i = 0; i < receiveList.num_files; i++) {
                         if (strcmp(receiveList.filenames[i], filename) == 0) {
-                            found = 1;
+                            find = 1;
                             break;
                         }
                     }
 
-                    if (found) {
-                        // Gửi tên tệp đến server và nhận tệp từ server
-                        send(socket_fd, filename, strlen(filename) + 1, 0);
+                    if (find) {
+                        snprintf(request, sizeof(request), "%s%s", "send", filename);
+                        printf("%s", request);
+                        send(socket_fd, request, strlen(request) + 1, 0);
+                        //send(socket_fd, filename, strlen(filename) + 1, 0);
                         receiveFile(socket_fd, filename);
                         t = 1;
                     } else {
                         printf("Tệp không tồn tại. Vui lòng thử lại.\n");
                     }
                 }
+		        close(socket_fd);
                 break;
             }
 
-            case 3:
+            case 3: {
+                char request[1024] = "upload";
+                socket_fd = socket(AF_INET, SOCK_STREAM, 0);
+                connect(socket_fd, (struct sockaddr *)&server_addr, sizeof(server_addr));
+                char password[256];
+                printf("Nhập password de tai tai lieu hoặc 'quit' để thoát: ");
+                fgets(password, sizeof(password), stdin);
+                
+                size_t len = strlen(password);
+                if (len > 0 && password[len - 1] == '\n') {
+                    password[len - 1] = '\0';
+                }
+
+                if (strcmp(password, "quit") == 0) {
+                    break;
+                }
+
+                char filename[256];
+                printf("Nhập ten file hoặc 'quit' để thoát: ");
+                fgets(filename, sizeof(filename), stdin);
+                size_t len1 = strlen(filename);
+                if (len1 > 0 && filename[len1 - 1] == '\n') {
+                    filename[len1 - 1] = '\0';
+                }
+
+                if (strcmp(filename, "quit") == 0) {
+                    break;
+                }
+
+                snprintf(request, sizeof(request), "%s%s%s", "upload", password, filename);
+                send(socket_fd, request, strlen(request) + 1, 0);
+
+                printf("%s", request);
+                //if(upload_confirm(socket_fd)) {
+                upload_file(socket_fd, filename);
+                 //}
+
+
+                // char filename[256];
+                // printf("Nhập ten file hoặc 'quit' để thoát: ");
+                // fgets(filename, sizeof(filename), stdin);
+                // size_t len = strlen(filename);
+                // if (len > 0 && filename[len - 1] == '\n') {
+                //     filename[len - 1] = '\0';
+                // }
+
+                // if (strcmp(filename, "quit") == 0) {
+                //     break;
+                // }
+
+                // snprintf(request, sizeof(request), "%s%s%s", "upload", password, filename);
+                // send(socket_fd, request, strlen(request) + 1, 0);
+
+
+
+                close(socket_fd);
+                break;
+            }
+
+            case 4:
                 close(socket_fd);
                 exit(0);
                 break;
@@ -157,7 +278,7 @@ int main() {
         }
     }
 
-    close(socket_fd);
+    //close(socket_fd);
 
     return 0;
 }
